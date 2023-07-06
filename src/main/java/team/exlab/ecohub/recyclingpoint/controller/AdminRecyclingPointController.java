ff8e962145a6d53fb6974ff014d5469419562937
nikitabulak <nikita_bulak@mail.ru>
2023-07-06 12:01:36 +0300

    feat: added keycloak-based authentication for admin and superadmin roles

--- src/main/java/team/exlab/ecohub/recyclingpoint/controller/AdminRecyclingPointController.java
+++ src/main/java/team/exlab/ecohub/recyclingpoint/controller/AdminRecyclingPointController.java
@@ -1,6 +1,8 @@
 package team.exlab.ecohub.recyclingpoint.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import team.exlab.ecohub.recyclingpoint.RecyclingPointService;
 import team.exlab.ecohub.recyclingpoint.dto.RecyclingPointDto;
@@ -13,7 +15,14 @@ public class AdminRecyclingPointController {
 
     @PostMapping
     public RecyclingPointDto createPoint(@RequestBody RecyclingPointDto recyclingPoint) {
+        printAuthenticationParams();
         return recyclingPointService.createPoint(recyclingPoint);
+    }
+
+    @GetMapping
+    public String get() {
+        printAuthenticationParams();
+        return "GetResponse";
     }
 
     @PatchMapping("/{pointId}")
@@ -25,5 +34,15 @@ public class AdminRecyclingPointController {
     @DeleteMapping("/{pointId}")
     public void deletePoint(@PathVariable Long pointId) {
         recyclingPointService.deletePoint(pointId);
+    }
+
+    private void printAuthenticationParams(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(auth.getAuthorities());
+        System.out.println(auth.getName());
+        System.out.println(auth.getPrincipal());
+        System.out.println(auth.getCredentials());
+        System.out.println(auth.getDetails());
+        auth.getAuthorities().forEach(System.out::println);
     }
 }
---
+++ src/main/java/team/exlab/ecohub/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package team.exlab.ecohub.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+class SecurityConfig {
+
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
+    private final JwtAuthConverter jwtAuthConverter;
+
+//    @Bean
+//    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+//        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/admin/**")
+                .hasAnyRole("admin", "superadmin")
+                .antMatchers("/superadmin/**")
+                .hasRole("superadmin")
+                .anyRequest()
+                .permitAll();
+        http.oauth2Login()
+                .and()
+                .logout()
+                .addLogoutHandler(keycloakLogoutHandler)
+                .logoutSuccessUrl("/");
+        http.oauth2ResourceServer().jwt()
+                .jwtAuthenticationConverter(jwtAuthConverter);
+        return http.build();
+    }
+}
