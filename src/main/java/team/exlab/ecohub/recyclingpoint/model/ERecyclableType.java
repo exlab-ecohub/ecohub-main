1b431354728858b9f653a3aa913d0c7a66548f4b
nikitabulak <nikita_bulak@mail.ru>
2023-09-25 12:48:45 +0300

    feat: added recycling types and some recycling points

--- src/main/java/team/exlab/ecohub/recyclingpoint/model/ERecyclableType.java
+++ src/main/java/team/exlab/ecohub/recyclingpoint/model/ERecyclableType.java
@@ -1,13 +1,15 @@
 package team.exlab.ecohub.recyclingpoint.model;
 
 public enum ERecyclableType {
-    PLASTIC,
-    PAPER,
-    GLASS,
-    BULKY_WASTE,
-    APPLIANCES,
-    HAZARDOUS_WASTE,
-    METAL,
-    CLOTHES,
-    SECOND_LIFE
+    PLASTIC,            //пластик
+    PAPER,              //бумага
+    GLASS,              //стекло
+    BULKY_WASTE,        //крупногабаритные отходы
+    APPLIANCES,         //электронная и бытовая техника
+    HAZARDOUS_WASTE,    //опасные отходы
+    METAL,              //металл
+    CLOTHES,            //одежда
+    SECOND_LIFE,        //вторая жизнь
+    RAGS,               //ветошь
+    SHOES               //обувь
 }
