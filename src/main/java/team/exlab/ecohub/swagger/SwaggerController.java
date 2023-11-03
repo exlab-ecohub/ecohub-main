81369fa9d2a2cb50d7e04d6c436bccb37453a6fb
nikitabulak <nikita_bulak@mail.ru>
2023-11-03 19:00:06 +0300

    feat: added endpoint to view documentation in Swagger

--- src/main/java/team/exlab/ecohub/swagger/SwaggerController.java
+++ src/main/java/team/exlab/ecohub/swagger/SwaggerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/swagger")
+@RequestMapping("/swagger-download")
 @RequiredArgsConstructor
 public class SwaggerController {
     private final SwaggerService swaggerService;
